afbe1be30c1cc60b8ff0717ee8e90315ca94737d
Sorin Turda <s.turda12@gmail.com>
2025-04-11 13:50:11 +0300

    added the initial project, generated with spring initializr

---
+++ backend/syncv/src/test/java/com/example/syncv/SyncvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syncv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
