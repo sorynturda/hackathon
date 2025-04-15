648c1e540ab42cda58c1e4e6aebd705950e9427c
sorynturda <138104462+sorynturda@users.noreply.github.com>
2025-04-15 11:33:12 +0300

    pull request for Paul's branch (#9)
    * trying to connect vercel
    
    * test
    
    * hero plain
    
    * responsive fix
    
    * about 1 plain
    
    * cateva sectiuni plain
    
    * cvs section plain
    
    * homepage plain fara responsiveness yet
    
    ---------
    
    Co-authored-by: St4r <pauloprisor2@gmail.com>
    Co-authored-by: St4r <111751798+StarGame1@users.noreply.github.com>

---
+++ frontend/syncv/app/(main)/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Hero from "./sections/Hero";
+import About1 from "./sections/About1";
+import About2 from "./sections/About2";
+import About3 from "./sections/About3";
+import Words from "./sections/Words";
+import ParallaxZoom from "./sections/ParallaxZoom";
+import Footer from "./sections/Footer";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Hero />
+      <About1 />
+      <About2 />
+      <About3 />
+      <Words />
+      <ParallaxZoom />
+      <Footer />
+    </div>
+  );
+}
