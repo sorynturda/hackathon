497911f64df8c2e1f9f4db2f7fc5cbfe5104a540
Sorin Turda <s.turda12@gmail.com>
2025-04-16 21:32:02 +0300

    added a redis server using docker. postgres database is also on a docker container

--- backend/syncv/src/main/java/com/example/syncv/config/SecurityConfig.java
+++ backend/syncv/src/main/java/com/example/syncv/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth ->
                         auth
-                                .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/auth/**", "/api/test/**").permitAll()
                                 .anyRequest().authenticated()
                 );
 
