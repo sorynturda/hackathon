5b4a43368bd2125853643e62cc30e9dfc4c636de
Sorin Turda <s.turda12@gmail.com>
2025-04-22 17:20:49 +0300

    api post to an external server. it works but it needs updates

--- backend/syncv/src/main/java/com/example/syncv/controller/MatchController.java
+++ backend/syncv/src/main/java/com/example/syncv/controller/MatchController.java
@@ -7,6 +7,8 @@ import com.example.syncv.model.dto.MatchResponseDTO;
 import com.example.syncv.model.entity.CV;
 import com.example.syncv.service.CVService;
 import com.example.syncv.service.JobDescriptionService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.security.core.Authentication;
@@ -40,13 +42,18 @@ public class MatchController {
             // aici se face request de pe react pentru un cv
             // si se va returna un jd
             // se face un request la API-ul de la serverul de fastAPI care va returna un JSON cu id-ul jd si scor
-            List<MatchResponseDTO> requestBody = new ArrayList<>();
+            MatchRequestDTO requestBody = new MatchRequestDTO(id, cvService.getCV(id).getUser().getId(), 5, inputs);
+
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<List<MatchResponseDTO>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+            HttpEntity<MatchRequestDTO> requestEntity = new HttpEntity<>(requestBody, headers);
+
+            ObjectMapper mapper = new ObjectMapper();
+            System.out.println("Request JSON: " + mapper.writeValueAsString(requestBody));
             ResponseEntity<?> res = restTemplate.exchange(
                     "http://host.docker.internal:8080/api/test",
-                    HttpMethod.GET,
+                    HttpMethod.POST,
                     requestEntity,
                     new ParameterizedTypeReference<List<MatchResponseDTO>>() {
                     }
@@ -55,7 +62,10 @@ public class MatchController {
             return ResponseEntity.ok(res.getBody());
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error: " + e.getMessage());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -65,9 +75,29 @@ public class MatchController {
             // aici se face request de ep react pentru jd-uri
             // se returneaza lista de jd-uri (dto momentan)
             // se face request la API-ul de la serverul de fastAPI care returneaza un JSON cu scor si id
-            return ResponseEntity.ok("Asd");
+            MatchRequestDTO requestBody = new MatchRequestDTO(id, jobDescriptionService.getJobDescription(id).getUser().getId(), 1, new ArrayList<>());
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<MatchRequestDTO> requestEntity = new HttpEntity<>(requestBody, headers);
+
+            ObjectMapper mapper = new ObjectMapper();
+            System.out.println("Request JSON: " + mapper.writeValueAsString(requestBody));
+            ResponseEntity<?> res = restTemplate.exchange(
+                    "http://host.docker.internal:8080/api/test2",
+                    HttpMethod.POST,
+                    requestEntity,
+                    new ParameterizedTypeReference<List<MatchResponseDTO>>() {
+                    }
+            );
+
+            return ResponseEntity.ok(res.getBody());
+
         } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
