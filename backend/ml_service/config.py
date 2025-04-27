8e5ea3ceb84d16643c8aab0a0705496741ddaf65
cocolino21 <mihneabostina5@gmail.com>
2025-04-28 00:16:02 +0300

    implemented matching (missing first 10% criteria) for both jd and cv | only 1 jd to many cvs matching in the correct format

---
+++ backend/ml_service/config.py
@@ -0,0 +1,5 @@
+
+MONGODB_CONFIG = {
+    "connection_string": "mongodb+srv://mihnea:1kZIOaKb9AXFVvTA@cluster0.ucqcya0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "db_name": "syncv_mdb"
+}
